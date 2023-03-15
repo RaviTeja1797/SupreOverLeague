2ce0ad8335022052fa00d7450cc9fbb18607b037
Ravi Teja <ravi_teja_1797@yahoo.com>
2023-03-15 16:32:15 +0000

    fixed the issues

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const imageLink1 = "https://assets.ccbp.in/frontend/react-js/rcb-img.png";
 const imageLink2 = "https://assets.ccbp.in/frontend/react-js/csk-img.png"
 
+const imageOne = <img src={imageLink1}></img>;
 
-const element = (
-<div className="root_container">
+
+const element = (<div className="root_container">
     <h1 className = 'heading'>Super Over league</h1>
     <div className = 'image-container'>
-        <img src={imageLink1}></img>
-        <img src={imageLink2}></img>
+        <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"/>
+        <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png"/>>
     </div>
 </div>
 );
